cdd6ad966d597561fda94dd808f8bcdb353d9d81
Lseoksee <da864268@naver.com>
2024-01-02 16:57:11 +0900

    앨범 json 처리

--- src/Component/App.tsx
+++ src/Component/App.tsx
@@ -1,48 +1,46 @@
 import React, { useState } from "react";
 import * as musicMetadata from "music-metadata-browser";
 import { getBlobToSize } from "../Utils/WebApi";
+import albumList from "../albumList.json";
+import { API_URL } from "../constants";
 
-const mediaParms = "uc?export=download&id=1Y6IvAa_57y3i-184Nuf0h5NbZDyEQE6z";
-
-type AppProps = {
-    data: string;
-    setState: React.Dispatch<React.SetStateAction<string | JSX.Element[]>>;
-};
-
-const GetMetadata = async (): Promise<JSX.Element[]> => {
-    const blob = await getBlobToSize(`media/${mediaParms}`, 512);
-
+const GetMetadata = async (url: string,list: JSX.Element[]): Promise<JSX.Element[]> => {
+    const blob = await getBlobToSize(url, 512);
     const metadata = await musicMetadata.parseBlob(blob);
-    const resArr: JSX.Element[] = [];
 
-    resArr.push(
+    list.push(
         <div>
             <h1>{metadata.common.title}</h1>
             <audio controls>
-                <source src={`media/${mediaParms}`}></source>
+                <source src={url}></source>
             </audio>
         </div>
     );
 
-    return resArr;
+    return list;
 };
 
-function SetMusic(props: AppProps): JSX.Element {
-    if (props.data === "avg") {
-        GetMetadata().then((data) => {
-            props.setState(data);
-        });
+function SetMusic(props: any): JSX.Element {
+    const [data, setState] = useState<string | JSX.Element[]>("loading");
+    const musicList: JSX.Element[] = [];
+
+    if (data === "loading") {
+        albumList[0].track.forEach(item => {
+            GetMetadata(`${API_URL}${item}`, musicList).then((data) => {
+                setState(data);
+            });
+        })
+
+        return <></>;
     }
 
-    return <div>{props.data}</div>;
+    return <div>{data}</div>;
 }
 
 function App(): JSX.Element {
-    const [data, setState] = useState<string | JSX.Element[]>("avg");
-
     return (
         <div>
-            <SetMusic setState={setState} data={data as string} />
+            <SetMusic />
         </div>
     );
 }
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+/* 앱 상수 정의 */
+
+/** 구글 드라이브 다운로드 api 요청 경로 */
+export const API_URL = "media/uc?export=download&id=";
