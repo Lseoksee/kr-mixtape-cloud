10b6e6ed67e7b64b76ebcf1c68c3d8d6963b9628
Lseoksee <da864268@naver.com>
2024-02-16 17:58:56 +0900

    전곡 재생 추가

--- src/Pages/ArtistPage.tsx
+++ src/Pages/ArtistPage.tsx
@@ -3,23 +3,55 @@ import AlbumView, { AlbumViewState } from "../Components/AlbumComponet";
 import "../Style/ArtistPage.css";
 import { AlbumCacheManager } from "../Utils/GlobalAppData";
 import { useLoaderData, useParams } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Utils from "../Utils/Utils";
 import tempArtist from "../Assets/tempArtist.svg";
 import { MUIComponet } from "../Style/StyleComponents/MUICustum";
 import { useDispatch } from "react-redux";
+import { ReduxActions } from "../Store/ConfingRedux";
+import AWSUtiil from "../Utils/AWSUtill";
 
 const ConstUtills = {
     SetMusicMemo: React.memo(SetMusic),
-    /* 아티스트 페이지 전체 로드 시 건네는 이벤트 */
-    albumComplete(stateData: AlbumViewState[]) {
-        console.log("모든앨범 로드완료");
-        console.log(stateData);        
+    
+    /* 전체 재생 하는거 loadMusicInfo 얻어오는 함수  */
+    async playAllalbumArr(loadAlbums: AlbumViewState[]) {
+        return await Promise.all(
+            loadAlbums
+                .map((album) =>
+                    album.playerElement.map(async (music) => {
+                        return {
+                            albumName: album.albumInfo.album,
+                            musicMeta: music,
+                            albumArtUrl: Utils.byteStringToBlob(album.albumInfo.art),
+                            url: await AWSUtiil.getAWSUtiil().getFileURL(music.file),
+                        } as AlbumCompType.loadMusicInfo;
+                    })
+                )
+                .reduce((a, b) => [...a, ...b])
+        );
     },
 };
 
-function SetMusic(props: { aritst: (typeof albumList)[0]; albums: AlbumCompType.file[] }): JSX.Element {
-    const albumCacheManager = new AlbumCacheManager(props.aritst.albums.length, ConstUtills.albumComplete);
+function SetMusic(props: {
+    aritst: (typeof albumList)[0];
+    albums: AlbumCompType.file[];
+    pageState: React.Dispatch<
+        React.SetStateAction<{
+            loadAlbums: AlbumViewState[];
+        }>
+    >;
+}): JSX.Element {
+    const albumCacheManager = new AlbumCacheManager(props.aritst.albums.length, (stateData) => {
+        /* 아티스트 페이지 전체 로드 시 건네는 이벤트 */
+        console.log("모든 앨범로드");
+        props.pageState((prev) => {
+            if (prev.loadAlbums !== stateData) {
+                return { loadAlbums: stateData };
+            }
+            return prev;
+        });
+    });
 
     const element = props.aritst.albums.map((item, index) => {
         // 해당 아티스트에 전체앨범 곡중 현재 로드중인 앨법에 곡만
@@ -41,10 +73,19 @@ function SetMusic(props: { aritst: (typeof albumList)[0]; albums: AlbumCompType.
 }
 
 function ArtistPage(): JSX.Element {
+    console.log("상위");
+
     const { artistName } = useParams<RouterType.RouterParams>();
+    const [state, setState] = useState<{
+        loadAlbums: AlbumViewState[];
+    }>({ loadAlbums: [] });
     //loader로 받은거 얻기
     const albums = useLoaderData() as AlbumCompType.file[];
     const dispatch = useDispatch();
+
+    const dispatchMusic = (actions: any) => {
+        dispatch(actions);
+    };
 
     const artist = albumList.find((itme) => itme.artist === artistName)!!;
 
@@ -65,6 +106,13 @@ function ArtistPage(): JSX.Element {
                         <div className="playDiv">
                             <MUIComponet.PlayIconFill
                                 sx={{ width: "initial", height: "2rem" }}
+                                onClick={async () => {
+                                    const dispatch = ReduxActions.setStartMusic({
+                                        loadMusicInfo: await ConstUtills.playAllalbumArr(state.loadAlbums),
+                                        startIndex: 0,
+                                    });
+                                    dispatchMusic(dispatch)
+                                }}
                             ></MUIComponet.PlayIconFill>
                             <p> 재생하기</p>
                         </div>
@@ -72,7 +120,12 @@ function ArtistPage(): JSX.Element {
                 </div>
                 <div className="artistAlbumlist"></div>
             </div>
-            <ConstUtills.SetMusicMemo aritst={artist} key={artist.artist} albums={albums}></ConstUtills.SetMusicMemo>
+            <ConstUtills.SetMusicMemo
+                aritst={artist}
+                key={artist.artist}
+                albums={albums}
+                pageState={setState}
+            ></ConstUtills.SetMusicMemo>
         </div>
     );
 }
